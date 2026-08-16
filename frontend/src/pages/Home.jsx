f128f5e6326b597597b3c07aaa8bd5aafbfc3fb0
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-08-16 10:58:06 -0300

    fix: remove pagina de valores

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,6 @@ export default function Home() {
             <Main />
             <Stats />
             <AboutServices />
-            <Services />
             <AboutDev />
             <Skills />
             <Projects />
